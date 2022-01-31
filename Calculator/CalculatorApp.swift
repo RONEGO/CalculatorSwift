68f37838004732722df7188e33a13955717bca61
Egor Geronin <egorgeronin@MacBook-Air-Egor.local>
2022-01-31 12:20:59 +0300

    Calculator

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Egor Geronin on 14.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
